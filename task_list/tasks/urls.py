5f064a97b995f5e44ad591820546a98af7327832
muskangarg16 <muskan.garg@djtcorp.in>
2024-09-06 17:41:42 +0530

    url changes

--- task_list/tasks/urls.py
+++ task_list/tasks/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 # from . import views
 urlpatterns = [
     # path('', views.registerfunc, name='register'),
-    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('tasks/', TaskListCreateAPIView.as_view(), name='tasks'),
     path('tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='task_detail'),
