6edc93edc0a006c40543a0ad1db3bda849c303b8
muskangarg16 <muskan.garg@djtcorp.in>
2024-09-10 13:20:10 +0530

    added whitenoise middleware

--- task_list/task_list/settings.py
+++ task_list/task_list/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,4 +154,4 @@ import os
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
